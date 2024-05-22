25aacd6d12d6d77281151585a102dea0914c391b
vignesvaran0823 <vignesvaran.g2023mech@sece.ac.in>
2024-05-22 10:20:11 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,6 @@
 # find if the given number is a palindrome or not?
+a=int(input( "Enter the first number: ")) 
+if a==a[::-1]:
+  print('given no is palindrome')
+else:
+  print('given no is not palindrome')
